41f5f6e3397093ae45498662a5f0a6e33890481d
2shlomi9 <2shlomiariel9@gmail.com>
2024-12-26 21:38:33 +0200

    first commit

---
+++ Perceptron.py
@@ -0,0 +1,98 @@
+import numpy as np
+import math
+
+
+def euclidean_distance(point1, point2):
+    return np.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+
+def found_margin(a, b, c):
+    # Found the vector ab
+    vector = np.array([b[0]-a[0],b[1]-a[1]])
+    # Found ortogonaly vector of ab
+    perpendicular_vector = np.array([vector[1],-vector[0]])
+    # Perpendicular vector length
+    magnitude_vec = np.linalg.norm(perpendicular_vector)
+    # Noramlization the Perpendicular vector
+    perpendicular_vector[0] = perpendicular_vector[0]/magnitude_vec
+    perpendicular_vector[1] = perpendicular_vector[1]/magnitude_vec
+    # Found the projection of point c on Perpendicular vector (distance)
+    distance_to_line = abs(np.dot(c, perpendicular_vector))
+    # The true margin
+    margin = distance_to_line / 2
+
+    return margin
+
+def brute_force(data_1,data_2):
+    
+    min_dist = 0
+    tmp_dist = 0
+
+    a = np.zeros(data_1.shape[1])
+    b = np.zeros(data_1.shape[1])
+    c = np.zeros(data_1.shape[1])
+
+    # Try all combinations of 3 points such that two points belong to data_1 and one to data_2.
+    for i in range(len(data_1)):
+        for j in range(i+1, len(data_1)):
+            for k in range(len(data_2)):
+                tmp_dist = euclidean_distance(data_1[i], data_2[k]) + euclidean_distance(data_1[j], data_2[k])
+                if i==0 and j==1 and k==0:
+                    min_dist = tmp_dist
+                    a = data_1[i]
+                    b = data_1[j]
+                    c = data_2[k]
+                elif tmp_dist < min_dist:
+                    min_dist = tmp_dist
+                    a = data_1[i]
+                    b = data_1[j]
+                    c = data_2[k]
+
+    # Try all combinations of 3 points such that two points belong to data_2 and one to data_1.
+    for i in range(len(data_2)):
+        for j in range(i+1, len(data_2)):
+            for k in range(len(data_1)):
+                tmp_dist = euclidean_distance(data_2[i], data_1[k]) + euclidean_distance(data_2[j], data_1[k])
+                if tmp_dist < min_dist:
+                    min_dist = tmp_dist
+                    a = data_1[i]
+                    b = data_1[j]
+                    c = data_2[k]
+
+    return found_margin(a, b, c)
+
+                
+
+def perceptron(data_1, data_2):
+    
+    y1 = np.ones(data_1.shape[0])  # All labels 1
+    y2 = -np.ones(data_2.shape[0])  # All labels -1
+
+    n = data_1.shape[0] + data_2.shape[0]  # Number of samples
+
+    X = np.vstack((data_1, data_2))  # Merging the two sets
+    Y = np.concatenate((y1, y2))  # Merge labels
+    w = np.zeros(data_1.shape[1])  # Initialize weight vector to zero
+
+    count_mistakes = 0
+    mistakes = True  # Variable to check if any mistakes are made in the epoch
+
+    while mistakes:
+        mistakes = False  # Reset mistakes flag at the start of each epoch
+
+        for i in range(n):
+            prediction = np.dot(w, X[i])  # Prediction for current sample
+
+            if prediction <= 0 and Y[i] == 1:  # Misclassified positive example
+                w += X[i]  # Update weights
+                mistakes = True
+                count_mistakes += 1
+            elif prediction > 0 and Y[i] == -1:  # Misclassified negative example
+                w -= X[i]  # Update weights
+                mistakes = True
+                count_mistakes += 1
+
+        # If no mistakes were made during the whole epoch, stop
+        if not mistakes:
+            break
+
+    return w, count_mistakes
---
+++ main.py
@@ -0,0 +1,125 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Perceptron import perceptron, brute_force
+
+
+
+def main(data_txt):
+
+    """
+    Part II - Perceptron algorithm
+
+    This part of the code runs the Perceptron algorithm on different sets of flower data.
+
+    Using the library - Perceptron:
+    - perceptron function: 
+        * return the weights vector (w)
+        * return the number of mistakes (num_of_mistakes) 
+    - compute_margin function:
+        * return the true maximum margin (c)
+
+    We perform classification on the following pairs of species:
+    a. Setosa and Versicolor
+    b. Setosa and Virginica
+    c. Versicolor and Virginica
+
+    """
+
+    # Read and write data from file to np array
+    setosa, versicolor, virginica = write_from_file(data_txt)
+
+    # Perceptron on Setosa and Versicolor
+    print("a - Perceptron on Setosa and Versicolor :")
+    w, num_of_mistakes = perceptron(setosa, versicolor) 
+    c, a, b, d = brute_force(setosa, versicolor) 
+    
+    plot_results(setosa, 'setosa', versicolor, 'versicolor', w, a, b, d, False, c)
+    plot_results(setosa, 'setosa', versicolor, 'versicolor', w, a, b, d, True, c)
+    print(f'Final weights vector : {w}')
+    print(f'Number of mistakes : {num_of_mistakes}')
+    print(f'True maximum margin : {c}')
+
+    # # Perceptron on Setosa and virginica
+    # print("b - Perceptron on Setosa and virginica :")
+    # w, num_of_mistakes = perceptron(setosa, virginica)
+    # c = brute_force(setosa, virginica)
+    
+    # plot_results(setosa, 'setosa', virginica, 'virginica', w)
+    # print(f'Final weights vector : {w}')
+    # print(f'Number of mistakes : {num_of_mistakes}')
+    # print(f'True maximum margin : {c}')
+
+    # # Perceptron on versicolor and virginica
+    # print("c - Perceptron on versicolor and virginica :")
+    # # w, num_of_mistakes = perceptron(versicolor, virginica)  ---->  Infinte loop, cannot be classified linearly
+    # # c = brute_force(versicolor, virginica)
+    
+    # print("Run time error - Cannot be classified linearly")
+    # plot_results(versicolor, 'versicolor', virginica, 'virginica', w)
+
+    """
+    Part III - Adaboost algorithm
+
+    This part of the code runs the Perceptron algorithm on different sets of flower data.
+    We perform classification on the following pairs of species:
+    a. Setosa and Versicolor
+    b. Setosa and Virginica
+    c. Versicolor and Virginica
+    
+    """
+
+
+def write_from_file(data_txt):
+    setosa = []
+    versicolor = []
+    virginica = []
+    with open(data_txt, 'r') as file:
+
+        for line in file:
+            columns = line.split()  
+            if columns[4] == 'Iris-setosa':
+                setosa.append([float(columns[1]), float(columns[2])])
+            if columns[4] == 'Iris-versicolor':
+                versicolor.append([float(columns[1]), float(columns[2])])
+            if columns[4] == 'Iris-virginica':
+                virginica.append([float(columns[1]), float(columns[2])])
+    setosa = np.array(setosa)
+    versicolor = np.array(versicolor)
+    virginica = np.array(virginica)
+
+    return setosa, versicolor, virginica
+
+def plot_results(data_1, name_1, data_2, name_2, w, a, b, c, booli, margin):
+    # Plot the data points
+    if booli:
+        plt.scatter(data_1[:, 0], data_1[:, 1], color='red', label=f'Class 1 {name_1}')
+        plt.scatter(data_2[:, 0], data_2[:, 1], color='blue', label=f'Class 2 {name_2}')
+    else:
+        plt.scatter(a[0], a[1], color='red', label=f'Class 1 {name_1}')
+        plt.scatter(b[0], b[1], color='red', label=f'Class 1 {name_1}')
+        plt.scatter(c[0], c[1], color='blue', label=f'Class 2 {name_2}')
+    # Calculate the decision boundary: w1*x + w2*y + w0 = 0
+    # Let's assume w = [w1, w2], so the line is w1*x + w2*y = 0
+    # => y = -(w1/w2) * x (this is the decision boundary)
+    if name_1 == 'setosa':
+
+        print(data_1.shape)
+        
+        x_vals = np.linspace(min(data_1[:, 0].min(), data_2[:, 0].min()), max(data_1[:, 0].max(), data_2[:, 0].max()))
+        y_vals =-(w[0]/w[1]) * x_vals  # Solving for y in terms of x using the hyperplane equation
+        x_vals1 = np.linspace(min(a[0], b[0]), max(a[0], b[0]))
+        y_vals1 = np.linspace(min(a[1]+margin, b[1]+margin), max(a[1]+margin, b[1]+margin))  # Solving for y in terms of x using the hyperplane equation
+        # y_vals1 = np.linspace(min(a[1], b[1]), max(a[1], b[1])) 
+        plt.plot(x_vals1, y_vals1, color='green', label='Decision boundary (Perceptron)')
+        plt.plot(x_vals, y_vals, color='black', label='Decision boundary (Perceptron)')
+
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.legend()
+    plt.title('Perceptron: Decision Boundary')
+    plt.show()
+
+if __name__ == "__main__":
+
+    DATA_PATH = 'iris.txt'  # Update with the path to your data
+    main(DATA_PATH)
